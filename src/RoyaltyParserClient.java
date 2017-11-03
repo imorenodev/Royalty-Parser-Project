c35edbf4cc7eeaefb84fb44533a5a74d45584e33
Ian Moreno <imorenodev@gmail.com>
2017-11-03 10:55:43 -0400

    cleaned up Client class

--- src/RoyaltyParserClient.java
+++ src/RoyaltyParserClient.java
@@ -3,20 +3,17 @@ import java.util.*;
 public class RoyaltyParserClient {
 
     public static void main(String[] args) {
+    		// pre-saved authors
     		String[] authors = new String[] {"Brittany", "Claire", "Jeanne", "Katrina", "Nicole", "Rebecca"};
     		ArrayList<Author> authorsList = new ArrayList<>();
   
-		//List<Book> amazonRawData = RetailerRawData.getData("KDPRoyalties.xlsx");
-		
+    		// build list of pre-saved authors
     		for (String author : authors) {
-    			//String reportName = author.toString() + "'s Amazon Report";
     			Author a = new Author(author.toString());
     			authorsList.add(a);
-    			//AuthorReport amazonReport = new AuthorReport(reportName, amazonRawData, a.getASINList());
-    			//System.out.println(a);
-    			//System.out.println(amazonReport);
     		}
     		
+    		// present GUI to user
     		new UserGUI(authorsList);
     }
 }
